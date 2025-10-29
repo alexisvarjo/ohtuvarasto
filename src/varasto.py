baa70c4be0f3012c9ffea56e5350f94152ed49de
Alexis <alexisvarj@gmail.com>
2025-10-29 19:11:00 +0200

    gha testi

--- src/varasto.py
+++ src/varasto.py
@@ -1,6 +1,7 @@
 class Varasto:
     def __init__(self, tilavuus, alku_saldo=0):
         self.tilavuus = tilavuus
+        # "rint("höh")
         """if tilavuus > 0.0:
             self.tilavuus = tilavuus
         else:
