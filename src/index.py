8584ea883be8dbce0e5e86954e7a97e8115fb6c8
Alexis <alexisvarj@gmail.com>
2025-11-08 14:56:58 +0200

    tehtävä 6

--- src/index.py
+++ src/index.py
@@ -1,19 +1,28 @@
 from varasto import Varasto
 
 
-def main():
-    mehua = Varasto(100.0)
-    olutta = Varasto(100.0, 20.2)
-
+def init_varasto(
+    mehu_volyymi: float,
+    mehu_saldo: int = 0,
+    olut_volyymi: float = 0.0,
+    olut_saldo: int = 0,
+):
+    mehua = Varasto(mehu_volyymi, mehu_saldo)
+    olutta = Varasto(olut_volyymi, olut_saldo)
     print("Luonnin jälkeen:")
     print(f"Mehuvarasto: {mehua}")
     print(f"Olutvarasto: {olutta}")
+    return mehua, olutta
 
+
+def beer_getters(olutta):
     print("Olut getterit:")
     print(f"saldo = {olutta.saldo}")
     print(f"tilavuus = {olutta.tilavuus}")
     print(f"paljonko_mahtuu = {olutta.paljonko_mahtuu()}")
 
+
+def mehu_setters(mehua):
     print("Mehu setterit:")
     print("Lisätään 50.7")
     mehua.lisaa_varastoon(50.7)
@@ -22,15 +31,8 @@ def main():
     mehua.ota_varastosta(3.14)
     print(f"Mehuvarasto: {mehua}")
 
-    print("Virhetilanteita:")
-    print("Varasto(-100.0);")
-    huono = Varasto(-100.0)
-    print(huono)
 
-    print("Varasto(100.0, -50.7)")
-    huono = Varasto(100.0, -50.7)
-    print(huono)
-
+def scenario1_test(olutta, mehua):
     print(f"Olutvarasto: {olutta}")
     print("olutta.lisaa_varastoon(1000.0)")
     olutta.lisaa_varastoon(1000.0)
@@ -41,17 +43,44 @@ def main():
     mehua.lisaa_varastoon(-666.0)
     print(f"Mehuvarasto: {mehua}")
 
+
+def scenario2_test(olutta):
     print(f"Olutvarasto: {olutta}")
     print("olutta.ota_varastosta(1000.0)")
     saatiin = olutta.ota_varastosta(1000.0)
     print(f"saatiin {saatiin}")
     print(f"Olutvarasto: {olutta}")
 
+
+def scenario3_test(mehua):
     print(f"Mehuvarasto: {mehua}")
     print("mehua.otaVarastosta(-32.9)")
     saatiin = mehua.ota_varastosta(-32.9)
     print(f"saatiin {saatiin}")
     print(f"Mehuvarasto: {mehua}")
+
+
+def scenario_virhetilanne():
+    print("Virhetilanteita:")
+    print("Varasto(-100.0);")
+    huono = Varasto(-100.0)
+    print(huono)
+
+    print("Varasto(100.0, -50.7)")
+    huono = Varasto(100.0, -50.7)
+    print(huono)
+
+
+def main():
+    mehua, olutta = init_varasto(100.0, 0, 100.0, 20)
+    beer_getters(olutta)
+    mehu_setters(mehua)
+
+    scenario_virhetilanne()
+
+    scenario1_test(olutta, mehua)
+    scenario2_test(olutta)
+    scenario3_test(mehua)
 
 
 if __name__ == "__main__":
